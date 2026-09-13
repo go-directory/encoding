852a8d32edc1562a841cd38b21223b3a6e864924
JesseCoretta <74126726+JesseCoretta@users.noreply.github.com>
2026-09-13 14:40:07 -0700

    tag update

--- asn1/tag.go
+++ asn1/tag.go
@@ -17,8 +17,8 @@ const (
 )
 
 const (
-	ClassUniversal       = 0x0
-	ClassApplication     = 0x1
-	ClassContextSpecific = 0x2
-	ClassPrivate         = 0x3
+    ClassUniversal       = 0
+    ClassApplication     = 1
+    ClassContextSpecific = 2
+    ClassPrivate         = 3
 )
